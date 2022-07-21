aeab73dc46f1d6b267cb2946bf0ffb3a1c7eecd2
ChrisMcDan <chrismcdan97@gmail.com>
2022-07-21 09:08:37 -0500

    First commit

---
+++ Daily/July/7-21-2022/src/main/java/com/daily/July_21_2022/CustomException.java
@@ -0,0 +1,20 @@
+package com.daily.July_21_2022;
+
+public class CustomException extends Exception
+{
+	public CustomException(String s)
+	{
+		super(s);
+	}
+}
+
+class Main
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			throw new CustomException("Exception");	//SSingle line explicitly.
+		}catch(CustomException e) {System.out.println(e.getMessage());}
+	}
+}
