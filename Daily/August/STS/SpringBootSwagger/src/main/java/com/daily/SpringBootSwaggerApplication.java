b07f63043d1e58221e0a6e2492aa94a6d069e715
ChrisMcDan <chrismcdan97@gmail.com>
2022-08-16 09:57:30 -0500

    Add date

--- Daily/August/STS/SpringBootSwagger/src/main/java/com/daily/SpringBootSwaggerApplication.java
+++ Daily/August/STS/SpringBootSwagger/src/main/java/com/daily/SpringBootSwaggerApplication.java
@@ -1,13 +1,15 @@
+/*August 12, 2022*/
+
 package com.daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSwaggerApplication {
-
-	public static void main(String[] args) {
+public class SpringBootSwaggerApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringBootSwaggerApplication.class, args);
 	}
-
 }
